24bc8e51be3ee5d4c8beb2aadcf8f64761158691
Stefan Weissenberger <weissenberger.stefan@bcg.com>
2021-02-17 18:08:54 +0100

    add basic picking

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import World from "./World";
+import { useEffect, useState } from "react";
+import Controls from "./Controls";
+import Info from "./Info";
+import { resetCube } from "../3d/init";
+import { initCannon, initThree, animate } from "../3d/init";
 
 const App = () => {
+  const [pips, setPips] = useState(null);
+
+  useEffect(() => {
+    initCannon();
+    initThree(setPips);
+    animate();
+  }, []);
+
   return (
     <div className="app">
-      Hello World
-      <World />
+      <Controls resetCube={resetCube} />
+      <Info pips={pips} />
     </div>
   );
 };
